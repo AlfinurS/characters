7c371a5d122f6a17919ed8c378b7de6adafd3d9b
AlfinurS <aaalfinur87@gmail.com>
2023-07-09 10:51:36 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <HeaderComponent></HeaderComponent>
+    <main>
+      <router-view></router-view>
+    </main>
+    <FooterComponent></FooterComponent>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HeaderComponent from "@/components/HeaderComponent.vue";
+import FooterComponent from "@/components/FooterComponent.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    HeaderComponent,
+    FooterComponent,
+  },
+});
+</script>
+
+<style lang="scss">
+@import "@/assets/scss/style.scss";
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="header"></header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HeaderComponent",
+  components: {},
+
+  data() {
+    return {
+      isShowMenu: false,
+    };
+  },
+});
+</script>
